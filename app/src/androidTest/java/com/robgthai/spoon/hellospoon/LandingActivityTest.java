39d88fd9bdaa1ef8082841e484aff62761858ef0
RobGThai <thisisrobg@gmail.com>
2015-02-01 17:20:46 +0700

    Add Test to check if the new activity is EchoActivity

--- app/src/androidTest/java/com/robgthai/spoon/hellospoon/LandingActivityTest.java
+++ app/src/androidTest/java/com/robgthai/spoon/hellospoon/LandingActivityTest.java
@@ -1,16 +1,20 @@
 package com.robgthai.spoon.hellospoon;
 
 import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.espresso.NoMatchingViewException;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
 import android.view.View;
 
+import com.robgthai.spoon.hellospoon.echo.EchoActivity;
 import com.squareup.spoon.Spoon;
 
 import junit.framework.Assert;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -18,6 +22,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 
 public class LandingActivityTest extends ActivityInstrumentationTestCase2<LandingActivity>{
+
+    private static final int MAXIMUM_TIMEOUT = 5000; // Because 5 seconds is ANR limit
 
     private Activity mActivity;
 
@@ -56,12 +62,27 @@ public class LandingActivityTest extends ActivityInstrumentationTestCase2<Landin
         onView(withId(R.id.txtHello)).check(matches(withText("Hello world!")));
     }
 
-    public void test_click_txtHello_shouldOpen_EchoActivity() {
+    public void test_click_txtHello_shouldRemove_LandingFragment() {
         Spoon.screenshot(mActivity, "Hello_World_before_click");
         onView(withId(R.id.txtHello)).perform(click());
-        View v = mActivity.findViewById(R.id.txtHello);
         Spoon.screenshot(mActivity, "Hello_World_after_click");
-        Assert.assertNull("txtHello should no longer exists", v);
+        onView(withId(R.id.txtHello)).check(doesNotExist());
+    }
+
+    public void test_click_txtHello_shouldOpen_EchoActivity() {
+        Instrumentation.ActivityMonitor am =
+                getInstrumentation().addMonitor(EchoActivity.class.getName(), null, false);
+
+        Spoon.screenshot(mActivity, "Hello_World_before_click");
+        onView(withId(R.id.txtHello)).perform(click());
+
+        Activity echoActivity = am.waitForActivityWithTimeout(MAXIMUM_TIMEOUT);
+        Assert.assertNotNull("No new activity launched", echoActivity);
+        Spoon.screenshot(echoActivity, "EchoActivity");
+        Assert.assertEquals("EchoActivity is not called", 1, am.getHits());
+        Assert.assertEquals("New Activity is not EchoActivity", EchoActivity.class, echoActivity.getClass());
+
+        getInstrumentation().removeMonitor(am);
     }
 
 }
