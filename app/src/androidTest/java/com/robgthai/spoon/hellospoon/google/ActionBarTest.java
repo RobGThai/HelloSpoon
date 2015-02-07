895d13216d2b843c34946d55970060a004477625
RobGThai <thisisrobg@gmail.com>
2015-02-07 20:29:33 +0700

    Add Spoon screenshots

--- app/src/androidTest/java/com/robgthai/spoon/hellospoon/google/ActionBarTest.java
+++ app/src/androidTest/java/com/robgthai/spoon/hellospoon/google/ActionBarTest.java
@@ -9,6 +9,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 import com.robgthai.spoon.hellospoon.R;
+import com.squareup.spoon.Spoon;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -19,70 +20,95 @@ import android.test.suitebuilder.annotation.LargeTest;
  */
 @LargeTest
 public class ActionBarTest extends ActivityInstrumentationTestCase2<ActionBarTestActivity> {
-  public ActionBarTest() {
-    // This constructor was deprecated - but we want to support lower API levels.
-    super(ActionBarTestActivity.class);
-  }
 
-  @Override
-  public void setUp() throws Exception {
-    super.setUp();
-    // Espresso will not launch our activity for us, we must launch it via getActivity().
-    getActivity();
-  }
+    private ActionBarTestActivity mActivity;
 
-  @SuppressWarnings("unchecked")
-  public void testClickActionBarItem() {
-    onView(withId(R.id.hide_contextual_action_bar))
-      .perform(click());
+    public ActionBarTest() {
+        // This constructor was deprecated - but we want to support lower API levels.
+        super(ActionBarTestActivity.class);
+    }
 
-    onView(withId(R.id.action_save))
-      .perform(click());
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        // Espresso will not launch our activity for us, we must launch it via getActivity().
+        mActivity = getActivity();
+    }
 
-    onView(withId(R.id.text_action_bar_result))
-      .check(matches(withText("Save")));
-  }
+    @SuppressWarnings("unchecked")
+    public void testClickActionBarItem() {
+        Spoon.screenshot(mActivity, "Default_state");
 
-  @SuppressWarnings("unchecked")
-  public void testClickActionModeItem() {
-    onView(withId(R.id.show_contextual_action_bar))
-      .perform(click());
+        onView(withId(R.id.hide_contextual_action_bar))
+                .perform(click());
+        Spoon.screenshot(mActivity, "Hide_ab_click");
 
-    onView((withId(R.id.action_lock)))
-      .perform(click());
+        onView(withId(R.id.action_save))
+                .perform(click());
+        Spoon.screenshot(mActivity, "Save_click");
 
-    onView(withId(R.id.text_action_bar_result))
-      .check(matches(withText("Lock")));
-  }
+        onView(withId(R.id.text_action_bar_result))
+                .check(matches(withText("Save")));
+        Spoon.screenshot(mActivity, "After_save");
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testClickActionModeItem() {
+        Spoon.screenshot(mActivity, "Default_state");
+
+        onView(withId(R.id.show_contextual_action_bar))
+                .perform(click());
+        Spoon.screenshot(mActivity, "Show_ab_click");
+
+        onView((withId(R.id.action_lock)))
+                .perform(click());
+        Spoon.screenshot(mActivity, "Lock_click");
+
+        onView(withId(R.id.text_action_bar_result))
+                .check(matches(withText("Lock")));
+        Spoon.screenshot(mActivity, "After_lock");
+    }
 
 
-  @SuppressWarnings("unchecked")
-  public void testActionBarOverflow() {
-    onView(withId(R.id.hide_contextual_action_bar))
-      .perform(click());
+    @SuppressWarnings("unchecked")
+    public void testActionBarOverflow() {
+        Spoon.screenshot(mActivity, "Default_state");
 
-    // Open the overflow menu from action bar
-    openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withId(R.id.hide_contextual_action_bar))
+                .perform(click());
+        Spoon.screenshot(mActivity, "Hide_ab_click");
 
-    onView(withText("World"))
-      .perform(click());
+        // Open the overflow menu from action bar
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        Spoon.screenshot(mActivity, "Open_overflow");
 
-    onView(withId(R.id.text_action_bar_result))
-      .check(matches(withText("World")));
-  }
+        onView(withText("World"))
+                .perform(click());
+        Spoon.screenshot(mActivity, "World_clicked");
 
-  @SuppressWarnings("unchecked")
-  public void testActionModeOverflow() {
-    onView(withId(R.id.show_contextual_action_bar))
-      .perform(click());
+        onView(withId(R.id.text_action_bar_result))
+                .check(matches(withText("World")));
+        Spoon.screenshot(mActivity, "After_world");
+    }
 
-    // Open the overflow menu from contextual action mode.
-    openContextualActionModeOverflowMenu();
+    @SuppressWarnings("unchecked")
+    public void testActionModeOverflow() {
+        Spoon.screenshot(mActivity, "Default_state");
 
-    onView(withText("Key"))
-      .perform(click());
+        onView(withId(R.id.show_contextual_action_bar))
+                .perform(click());
+        Spoon.screenshot(mActivity, "Show_ab_click");
 
-    onView(withId(R.id.text_action_bar_result))
-      .check(matches(withText("Key")));
-  }
+        // Open the overflow menu from contextual action mode.
+        openContextualActionModeOverflowMenu();
+        Spoon.screenshot(mActivity, "Open_overflow");
+
+        onView(withText("Key"))
+                .perform(click());
+        Spoon.screenshot(mActivity, "Key_clicked");
+
+        onView(withId(R.id.text_action_bar_result))
+                .check(matches(withText("Key")));
+        Spoon.screenshot(mActivity, "After_key");
+    }
 }
